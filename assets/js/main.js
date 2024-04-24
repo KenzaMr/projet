470125323c103cbab58bc923190e91d1c771286a
knz <kenza.mroudjae@outlook.com>
2024-04-24 15:35:29 +0200

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,6 @@
-const icon= document.querySelector("#burger")
-const nav = document.querySelector("nav");
+const icon= document.getElementById("burger")
+const nav = document.getElementById("nav");
 icon.addEventListener("click", function () {
-  // Si la nav a la class active on enleve class active
-  if (nav.classList.contains("active")) {
-    nav.classList.remove("active");
-    console.log(nav)
-  }
-  // sinon on ajoute la class active
-  else {
-    nav.setAttribute("class", "active");
-    console.log(nav)
-  }
+  console.log("cc")
+nav.classList.toggle("active")
 });
